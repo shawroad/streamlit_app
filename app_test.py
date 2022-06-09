7e203e6e675a8643665c425f9d120e07f717a36f
AI炼丹师 <luxiaonlp@163.com>
2022-06-09 14:22:31 +0800

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -2,15 +2,6 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
-import altair
-
-
-def matplotlib_in_streamlit():
-     # 可以将matplot画的图嵌入到页面内   下面三行就可实现  但是太丑了
-     fig = plt.figure()
-     plt.plot([1, 2, 3, 4], [21, 12, 32, 12])
-     st.pyplot(fig)
 
 
 if __name__ == '__main__':
